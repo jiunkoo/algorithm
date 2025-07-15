424f2a0faba745aacbea366c697e6562c259cb39
JIUNKOO <52573800+jiunkoo@users.noreply.github.com>
2025-07-16 07:37:44 +0900

    Refactor: Backtracking #2
    - 시간 초과를 해결

--- 2025/07/code/lc60_permutation_sequence.py
+++ 2025/07/code/lc60_permutation_sequence.py
@@ -9,21 +9,22 @@ from typing import List
 
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        return self.backtracking(n, k, "", [])[k-1]
+        nums = [i for i in range(1, n + 1)]
 
-    def backtracking(self, n: int, k: int, answer: str, answers: List[str]) -> str:
-        if len(answers) >= k:
-            return answers
+        return self.backtracking(n, k, nums, len(nums), "")
 
-        if len(answer) == n:
-            answers.append(answer)
-        
-        for i in range(1, n + 1):
-            if str(i) in answer:
-                continue
-            answer += str(i)
-            self.backtracking(n, k, answer, answers)
-            answer = answer[:-1]
-        
-        return answers
+    def backtracking (self, n: int, k: int, nums: List[int], totalNum: int, answer: str) -> str:
+        if n <= 1:
+            answer += str(nums.pop())
+            return answer
+
+        q = k // self.factorial(n - 1)
+        r = k % self.factorial(n - 1)
+        pos = q - 1 if r == 0 else q
+        answer += str(nums.pop(pos))
+
+        return self.backtracking(n - 1, r, nums, totalNum, answer)
+
+    def factorial(self, n: int) -> int:
+        return n if n <= 1 else n * self.factorial(n - 1)
 # @lc code=end
