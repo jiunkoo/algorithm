df4f7127478aae6f6f7f34f40a88561f8200af2b
JIUNKOO <jiunkoo0106@gmail.com>
2025-07-27 17:59:45 +0900

    Refactor: backtracking #4
    - wrong answer (4/9 testcases passed)

--- 2025/07/code/lc51_n_queens.py
+++ 2025/07/code/lc51_n_queens.py
@@ -9,49 +9,71 @@ from typing import List
 
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        board = [["#" for _ in range(n)] for _ in range(n)]
-        # for x in range(n):
-        #     for y in range(n):
-        #         isDistinct = self.backtracking(n, board, x, y)
-        #         if isDistinct:
-        #             board[x][y] = 'Q'
-        pos = {
-            "s": {"x": 0, "y": 0}, # start
-            "c": {"x": 5, "y": 7}, # current
-        }
-        self.backtracking(n, board, 0, 0, 1, 0, 0)
-        print(board)
+        answers = []
+        for x in range(n):
+            for y in range(n):
+                board = [["." for _ in range(n)] for _ in range(n)]
+                board[x][y] = "Q"
+                answer = self.check_8_directions(n, board, x, y)
+                if answer and answer not in answers:
+                    # print(x, y, answer)
+                    answers.append(answer)
+        
+        return answers
 
-        return 0
+    def check_8_directions(self, n: int, board: List[List[str]], ox: int, oy: int) -> List[str]:
+        count = 0
+        for sx in range(n):
+            for sy in range(n):
+                up = self.backtracking(n, board, sx, sy, 1, sx, sy, "UP")
+                dn = self.backtracking(n, board, sx, sy, 1, sx, sy, "DOWN")
+                lt = self.backtracking(n, board, sx, sy, 1, sx, sy, "LEFT")
+                rt = self.backtracking(n, board, sx, sy, 1, sx, sy, "RIGHT")
+                ul = self.backtracking(n, board, sx, sy, 1, sx, sy, "UP_LEFT")
+                dr = self.backtracking(n, board, sx, sy, 1, sx, sy, "DOWN_RIGHT")
+                ur = self.backtracking(n, board, sx, sy, 1, sx, sy, "UP_RIGHT")
+                dl = self.backtracking(n, board, sx, sy, 1, sx, sy, "DOWN_LEFT")
 
-# ['.', '.', '.', '.']
-# ['.', '.', '#', '#']
-# ['.', '#', '.', '#']
-# ['.', '#', '#', '.']
+                if up and dn and lt and rt and ul and dr and ur and dl:
+                    board[sx][sy] = "Q"
+                    count += 1
+        
+        if ox == 0 and oy == 4:
+            print([''.join(row) for row in board])
 
-# ['.', '#', '.', '.']
-# ['.', '#', '#', '#']
-# ['.', '#', '#', '#']
-# ['.', '#', '#', '.']
+        if count == n:
+            return [''.join(row) for row in board]
+        return []
 
-    def backtracking(self, n: int, board: List[List[str]], sx: int, sy: int, k: int, cx: int, cy: int) -> bool:
-        print(f"{k}, ({cx},{cy})")
+    def backtracking(self, n: int, board: List[List[str]], sx: int, sy: int, k: int, cx: int, cy: int, direct: str) -> bool:
         if cx < 0 or cx >= n or cy < 0 or cy >= n:
             return True
-        if board[cx][cy] == '.' or board[cx][cy] == 'Q':
-            return False
-        board[cx][cy] = '.'
-        condition = (
-            self.backtracking(n, board, sx, sy, k + 1, sx - k, sy) and
-            self.backtracking(n, board, sx, sy, k + 1, sx + k, sy) and
-            self.backtracking(n, board, sx, sy, k + 1, sx, sy - k) and
-            self.backtracking(n, board, sx, sy, k + 1, sx, sy + k) and
-            self.backtracking(n, board, sx, sy, k + 1, sx - k, sy - k) and
-            self.backtracking(n, board, sx, sy, k + 1, sx + k, sy + k) and
-            self.backtracking(n, board, sx, sy, k + 1, sx - k, sy + k) and
-            self.backtracking(n, board, sx, sy, k + 1, sx + k, sy - k)
-        )
-        # board[sx][sy] = temp
+        if board[cx][cy] == "Q":
+            if cx == sx and cy == sy:
+                pass
+            else:
+                return False
 
-        return condition
+        if direct == "UP":
+            cx = sx - k
+        elif direct == "DOWN":
+            cx = sx + k
+        elif direct == "LEFT":
+            cy = sy - k
+        elif direct == "RIGHT":
+            cy = sy + k
+        elif direct == "UP_LEFT":
+            cx = sx - k
+            cy = sy - k
+        elif direct == "DOWN_RIGHT":
+            cx = sx + k
+            cy = sy + k
+        elif direct == "UP_RIGHT":
+            cx = sx - k
+            cy = sy + k
+        elif direct == "DOWN_LEFT":
+            cx = sx + k
+            cy = sy - k
+    
+        return self.backtracking(n, board, sx, sy, k + 1, cx, cy, direct)
 # @lc code=end
