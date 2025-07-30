cacfd1b797eac9311581e442e3f9192f1359ca3c
JIUNKOO <52573800+jiunkoo@users.noreply.github.com>
2025-07-31 07:10:07 +0900

    Refactor: backtracking #5
    - 빈 값을 저장하고 빈칸이 적은 보드부터 탐색

--- 2025/07/code/lc37_sudoku_solver.py
+++ 2025/07/code/lc37_sudoku_solver.py
@@ -12,8 +12,8 @@ class Solution:
         self.rows = [set() for _ in range(9)]
         self.cols = [set() for _ in range(9)]
         self.blocks = [set() for _ in range(9)]
+        self.empty_cells = []
 
-        # 현재 보드 상태를 기반으로 set 초기화
         for i in range(9):
             for j in range(9):
                 val = board[i][j]
@@ -21,35 +21,34 @@ class Solution:
                     self.rows[i].add(val)
                     self.cols[j].add(val)
                     self.blocks[self.block_index(i, j)].add(val)
+                else:
+                    self.empty_cells.append((i, j))
 
-        self.backtracking(board)
+        self.backtracking(0, board)
 
     def block_index(self, i, j):
         return (i // 3) * 3 + (j // 3)
 
-    def backtracking(self, board: List[List[str]]) -> bool:
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] == '.':
-                    for num in map(str, range(1, 10)):
-                        block = self.block_index(i, j)
-                        if (
-                            num not in self.rows[i] and
-                            num not in self.cols[j] and
-                            num not in self.blocks[block]
-                        ):
-                            board[i][j] = num
-                            self.rows[i].add(num)
-                            self.cols[j].add(num)
-                            self.blocks[block].add(num)
+    def backtracking(self, idx: int, board: List[List[str]]) -> bool:
+        if idx == len(self.empty_cells):
+            return True
+        
+        i, j = self.empty_cells[idx]
+        block = self.block_index(i, j)
+        for num in map(str, range(1, 10)):
+            if num not in self.rows[i] and num not in self.cols[j] and num not in self.blocks[block]:
+                board[i][j] = num
+                self.rows[i].add(num)
+                self.cols[j].add(num)
+                self.blocks[block].add(num)
 
-                            if self.backtracking(board):
-                                return True
+                if self.backtracking(idx + 1, board):
+                    return True
 
-                            board[i][j] = '.'
-                            self.rows[i].remove(num)
-                            self.cols[j].remove(num)
-                            self.blocks[block].remove(num)
-                    return False
-        return True
+                board[i][j] = '.'
+                self.rows[i].remove(num)
+                self.cols[j].remove(num)
+                self.blocks[block].remove(num)
+
+        return False
 # @lc code=end
