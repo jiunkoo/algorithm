cc97e3172bf1ad658c7f7c64343abe8a2e7efdf5
JIUNKOO <52573800+jiunkoo@users.noreply.github.com>
2025-08-11 07:22:20 +0900

    Refactor: backtracking #5

--- 2025/07/code/lc2035_partition_array_into_two_arrays_to_minimize_sum_difference.py
+++ 2025/07/code/lc2035_partition_array_into_two_arrays_to_minimize_sum_difference.py
@@ -9,5 +9,32 @@ from typing import List
 
 class Solution:
     def minimumDifference(self, nums: List[int]) -> int:
-        pass
+        nums.sort()
+        subsets = []
+        self.backtracking(nums, [], subsets)
+
+        answer = 999999999
+        for i in range(len(subsets) // 2):
+            diff = abs(sum(subsets[i]) - sum(subsets[-(i + 1)]))
+            if diff <= answer:
+                answer = diff
+        
+        return answer
+
+    def backtracking(self, nums: List[int], subset: List[int], subsets: List[List[int]]) -> None:
+        if len(subset) <= len(nums):
+            sorted_subset = sorted(subset)
+            if sorted_subset not in subsets:
+                subsets.append(sorted_subset[:])
+
+        for num in nums:
+            if num <= max(subset, default = -99999999) and subset.count(num) == nums.count(num):
+                continue
+
+            subset.append(num)
+            self.backtracking(nums, subset, subsets)
+            subset.pop()
+        
+        subsets.sort(key=lambda x: (len(x), x))
+        return
 # @lc code=end
