8b04bd4b5c84b301557c336a13e5177fcdab5479
JIUNKOO <52573800+jiunkoo@users.noreply.github.com>
2025-08-24 18:06:04 +0900

    Refactor: backtracking #5

--- 2025/07/code/pg72411_menu_renewal.py
+++ 2025/07/code/pg72411_menu_renewal.py
@@ -8,7 +8,15 @@
 from typing import List
 
 class Solution:
-    def solution(self, orders: List[str], course: List[int]) -> List[str]:
+    def solution(self, orders, course):
         answer = []
+        for i in range(len(orders)):
+            for j in range(len(orders)):
+                if i == j or len(orders[j]) > max(course, default = 10) or orders[j] in answer:
+                    continue
+                if orders[i] in orders[j] or orders[j] in orders[i]:
+                    answer.append(orders[j])
+        answer.sort()
+
         return answer
 # @pg code=end
